4b8e41ca4008d5f3dbe57a3cb5cbf6a8f1551c24
João <joaogabriel9512@gmail.com>
2023-04-08 14:22:26 -0300

    mudança no header

---
+++ js/menu.js
@@ -0,0 +1,10 @@
+const button = document.querySelector(".button-mobile");
+const nav = document.querySelector(".links nav");
+
+button.addEventListener("click", () => {
+  if (nav.style.display === "none") {
+    nav.style.display = "block";
+  } else {
+    nav.style.display = "none";
+  }
+});
